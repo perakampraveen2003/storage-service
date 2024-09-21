bd4aa4b9a90b3d8473c6120c8996d72c369684e5
Rohith <manipiradhi@gmail.com>
2024-09-21 11:21:30 +0530

    welcome

--- src/main/java/com/miniproject/storage_service/StorageServiceApplication.java
+++ src/main/java/com/miniproject/storage_service/StorageServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StorageServiceApplication {
 
 	public static void main(String[] args) {
+		System.out.println("welcome");
 		SpringApplication.run(StorageServiceApplication.class, args);
 	}
 
