7b6bcb667ac1dac66777690020d184002b30348e
praveen <praveen@gmail.com>
2024-09-21 11:16:13 +0530

    initial commit

---
+++ src/test/java/com/miniproject/storage_service/StorageServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.miniproject.storage_service;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorageServiceApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
